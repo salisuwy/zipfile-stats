cd00b7a091c18fe4bbbe1c82120e4bfbd8c328dd
Salisu Wada <wadasalisu@gmail.com>
2024-03-18 12:43:14 +0000

    Update FileInfoExtractor.java
    Fix: FileInfoExtrator works with a zip file in the PWD without requiring full path

--- src/main/java/com/generis/FileInfoExtractor.java
+++ src/main/java/com/generis/FileInfoExtractor.java
@@ -6,17 +6,22 @@ public class FileInfoExtractor {
 
     public static String extractFilename(String fullPath) {
         String separator = File.separator; // OS dependent file separator
+        if (!fullPath.contains(separator)) {
+            return fullPath.replaceAll(".zip", "");
+        }
         return fullPath.substring(fullPath.lastIndexOf(separator) + 1)
                 .replaceAll(".zip", "");
-    }
 
+    }
 
     public static String extractPath(String fullPath) {
         String separator = File.separator; // OS dependent file separator
+        if (!fullPath.contains(separator)) {
+            return new File(".").getAbsolutePath();
+        }
         return fullPath.substring(0, fullPath.lastIndexOf(separator));
     }
 
-    
     public static String makeOutputFilename(String fullPath) {
         String separator = File.separator; // OS dependent file separator
         String outFileName = FileInfoExtractor.extractFilename(fullPath);
